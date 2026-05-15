67e4f6ae56d2486849afe7012a9620b7d79ef4ee
Rajat <136880969+ShadyGit2004@users.noreply.github.com>
2026-05-15 17:12:19 +0530

    Update EmpDashboard.jsx

--- Frontend/src/components/EmpDashboard.jsx
+++ Frontend/src/components/EmpDashboard.jsx
@@ -1,5 +1,5 @@
 import { ArrowRightIcon, CalendarIcon, DollarSignIcon, FileTextIcon } from 'lucide-react';
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const EmpDashboard = ({data}) => {
