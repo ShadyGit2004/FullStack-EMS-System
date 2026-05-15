9fd0e7f19f440316fc9f6746e494bb986df1ebb8
Rajat <136880969+ShadyGit2004@users.noreply.github.com>
2026-05-15 14:53:12 +0530

    Update ui Layout.jsx

--- Frontend/src/pages/Layout.jsx
+++ Frontend/src/pages/Layout.jsx
@@ -13,7 +13,7 @@ const Layout = () => {
   
 
   return (
-    <div className="flex h-screen bg-green-300">      
+    <div className="flex h-screen bg-blue-300">      
       <Sidebar/>
       <main className="flex-1 overflow-y-auto">
         <div className="p-4 pt-16 sm:pt-6 lg:p-8 max-w-400 mx-auto">
